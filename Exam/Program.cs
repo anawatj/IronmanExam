99a1a9527f1e88b64f8457f3cf0a1fe2a0950ac9
anawatj <ajarusiripot@gmail.com>
2024-09-17 08:01:59 +0700

    Add Case 222 2 22

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -4,10 +4,9 @@ namespace Exam
 {
     internal class Program
     {
-      
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(MobilePad.OldPhonePad("222 2 22"));
             Console.WriteLine(MobilePad.OldPhonePad("33#"));
             Console.WriteLine(MobilePad.OldPhonePad("227*#"));
             Console.WriteLine(MobilePad.OldPhonePad("4433555 555666#"));
